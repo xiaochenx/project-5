9e7a31a0b71362d8b12cc4a17189c2837d35b580
Xiaochen Xu <xiaochex@gmail.com>
2021-08-03 23:49:09 -0400

    added comment form validation to ensure only good data in the backend

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,11 @@ class CommentsController < ApplicationController
 
     def create
         comment = Comment.create(comment_params)
-        render json: comment, status: :created
+        if comment.valid?
+             render json: comment, status: :created
+        else
+            render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+        end  
     end
 
     private
