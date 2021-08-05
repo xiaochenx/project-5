9cf8bcf2011e9e0cae17c6a2189777eb4362c807
Xiaochen Xu <xiaochex@gmail.com>
2021-08-05 19:36:46 -0400

    updated format of Navbar buttons

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -7,55 +7,47 @@ const Navbar = (props) => {
         return (
             <div>
                 <h1>Hello {props.user.username}</h1>
-                <Button animated compact color='red' size='samll' onClick={props.logoutUser}>
+                <Button  animated floated='right' compact color='red' size='samll' onClick={props.logoutUser}>
                     <Button.Content visible>Logout</Button.Content>
                      <Button.Content hidden>
                         <Icon name='user x' />
                      </Button.Content>
                 </Button>
                 
-                <br/>
-                {/* <Link to="/listing/all">
-                    <button className="submit-button">Marketplace</button>
-                </Link> */}
-                <Button animated  color='vk' as={Link} to="/listing/all">
+              
+                <Button floated='left' animated='fade'  color='vk' as={Link} to="/listing/all">
                     <Button.Content visible>Marketplace</Button.Content>
                     <Button.Content hidden>
                         <Icon name='exchange' />
                     </Button.Content>
                 </Button>
                 
-                <br />
-                {/* <Link to="/listings">
-                    <button className="submit-button">Your items for sale</button>
-                </Link> */}
-                <Button animated primary as={Link} to="/listings">
+                
+               
+                <Button floated='left' animated='fade' primary as={Link} to="/listings">
                     <Button.Content visible>Your items for sale</Button.Content>
                     <Button.Content hidden>
                         <Icon name='dollar sign' />
                     </Button.Content>
                 </Button>
-                <hr/>
+                
             </div>
         )
     } else{
         return (
             <div>
                 <br/>
-                {/* <Link to="/signup">
-                    <button className="submit-button">Signup</button>
-                </Link> */}
-                <Button animated color='orange' as={Link} to="/signup">
+
+              
+                <Button floated='right' animated='fade' color='orange' as={Link} to="/signup">
                     <Button.Content visible>Signup</Button.Content>
                         <Button.Content hidden>
                          <Icon name='signup' />
                         </Button.Content>
                 </Button>
-                <br/>
-                {/* <Link to="/login">
-                    <button className="submit-button">Login</button>
-                </Link> */}
-                <Button animated color='blue' as={Link} to="/login">
+                
+                
+                <Button floated='right' animated='fade' color='blue' as={Link} to="/login">
                 <Button.Content visible>Login</Button.Content>
                         <Button.Content hidden>
                             <Icon name='user' />
